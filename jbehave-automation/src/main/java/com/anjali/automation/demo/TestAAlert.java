0b3d552202ef18258767668377e900a49b72d0e5
Mahesh <biradar.m2@gmail.com>
2017-04-05 02:40:37 +0200

    first commit

---
+++ jbehave-automation/src/main/java/com/anjali/automation/demo/TestAAlert.java
@@ -0,0 +1,32 @@
+package com.anjali.automation.demo;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.steps.CandidateSteps;
+
+import com.anjali.automation.demo.serenity.AlertStep;
+import com.anjali.automation.demo.steps.MainStep;
+
+import net.serenitybdd.jbehave.SerenityStories;
+
+public class TestAAlert extends SerenityStories {
+
+	public List<String> storyPaths() {
+		StoryFinder finder = new StoryFinder();
+		return finder.findPaths(codeLocationFromClass(this.getClass()).getFile(), Arrays.asList("**/*.story"), null);
+	}
+
+	public static void main(String[] args) {
+
+		Embedder embedder = new Embedder();
+		List<String> pathsFound = new TestAAlert().storyPaths();
+		embedder.candidateSteps().add((CandidateSteps) new MainStep());
+		embedder.runStoriesAsPaths(pathsFound);
+	}
+
+}
---
+++ jbehave-automation/src/main/java/com/anjali/automation/demo/steps/MainStep.java
@@ -0,0 +1,34 @@
+package com.anjali.automation.demo.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import com.anjali.automation.demo.serenity.AlertStep;
+
+import net.thucydides.core.annotations.Steps;
+
+/**
+ */
+public class MainStep extends org.jbehave.core.steps.Steps{
+
+	@Steps
+	AlertStep alertStep;
+
+	@Given("enter web-address")
+	public void openHomePage() throws InterruptedException {
+		alertStep.open_page();
+		Thread.sleep(10000L);
+	}
+
+	@When("click on try button")
+	public void When_type_word() throws InterruptedException {
+		this.alertStep.click_try_button();
+	}
+
+	@Then("alert get appear to handle")
+	public void handle_alert1() throws InterruptedException {
+		System.out.println("In New Alert");
+		alertStep.call_alert();
+	}
+    }
